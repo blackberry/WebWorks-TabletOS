4662d28d05df2f1d7c79bdfd7bd7799f7cdf848c
Carolina Pinzon <cpinzon@rim.com>
2011-03-11 15:48:43 -0500

    Changes to invoke dispatcher and namespace. Any references and implementation for maps and update uris are removed because these are no longer supported

--- js_api/BlackBerry_Invoke/js/common/invoke_dispatcher.js
+++ js_api/BlackBerry_Invoke/js/common/invoke_dispatcher.js
@@ -23,12 +23,11 @@
 	var APP_URL_CAMERA = "camera://";
 	var APP_URL_CAMERA_VIDEO = "camera://video";
 	var APP_URL_BROWSER = "http://";
-	var APP_URL_MAP = "map://";
 	var APP_URL_MUSIC = "music://";
 	var APP_URL_PHOTOS = "photos://";
 	var APP_URL_VIDEOS = "videos://";
 	var APP_URL_APPWROLD = "appworld://"
-	var APP_URL_UPDATE = "update://";
+
 
 	var APP_TYPE_ERROR = "appType not supported";
 	var APP_BROWSER_ERROR = "Protocol specified in the url is not supported.";
@@ -49,13 +48,9 @@
 					remote.addParam(APP_TYPE,APP_URL_CAMERA);
 				break;
 				
-			//Maps
-			case 5:
-				remote.addParam(APP_TYPE,APP_URL_MAP);						
-				break;
-				
+
 			//Browser
-			case 11:	
+			case 10:	
 				
 				if(!args){
 					remote.addParam(APP_TYPE, APP_URL_BROWSER);
@@ -71,30 +66,25 @@
 				break;
 				
 			//Music
-			case 13:						
+			case 12:						
 				remote.addParam(APP_TYPE, APP_URL_MUSIC);
 				break;
 			
 			//Photos
-			case 14:
+			case 13:
 				remote.addParam(APP_TYPE, APP_URL_PHOTOS);
 				break;
 			
 			//Videos
-			case 15:
+			case 14:
 				remote.addParam(APP_TYPE, APP_URL_VIDEOS);
 				break;
 			
 			//AppWorld
-			case 16:
+			case 15:
 				remote.addParam(APP_TYPE, APP_URL_APPWROLD);
 				break;
 			
-			//Update
-			case 17:
-				remote.addParam(APP_TYPE, APP_URL_UPDATE);
-				break;
-				
 			default:
 				throw APP_TYPE_ERROR;
 		}
